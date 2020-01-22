27b54995fbf6708b957b79c57eb64350007354d8
Placido Wang <placidow@gmail.com>
2020-01-22 03:10:22 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,5 +1,8 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require_relative './directors_database'
+require 'pp'
+
+# pp directors_database
 
 def directors_totals(source)
   result = {}
@@ -11,6 +14,8 @@ def directors_totals(source)
   end
   result
 end
+
+# p directors_totals(directors_database)
 
 def gross_for_director(d)
   total = 0
@@ -25,7 +30,13 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  result = []
+  
+  for director in source do
+    result << director[:name]
+  end
+  
+  return result
 end
 
 def total_gross(source)
@@ -38,6 +49,61 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  total = 0
+  
+  directors = list_of_directors(source)
+  
+  #raul
+  # d_totals = directors_totals(source)
+
+  # directors.each do | d, index |
+  #   d_totals[d]
+  # end
+  
+  # pp directors
+  
+  d_totals = directors_totals(source)
+  
+  i = 0
+  while i < directors.length do
+    name = directors[i]
+    director_gross = d_totals[name]
+    
+    total += director_gross
+    
+    i += 1
+  end
+  
+  # total = get_gross_total(d_totals)
+  
+  return total
+  
 end
 
+# def get_gross_total(source)
+#   total = 0
+  
+#   for director in source do
+#     total += director[1]
+    
+#     # pp director[1]
+#   end
+  
+#   return total
+# end
 
+# def get_gross_total(director_totals)
+  
+# end
+
+# total_gross(directors_database)
+# p get_gross_total({"Stephen Spielberg"=>1357566430,
+# "Russo Brothers"=>2281002470,
+# "James Cameron"=>2571020373,
+# "Spike Lee"=>256624217,
+# "Wachowski Siblings"=>806180282,
+# "Robert Zemeckis"=>1273838385,
+# "Quentin Tarantino"=>662738268,
+# "Martin Scorsese"=>636812242,
+# "Francis Ford Coppola"=>509719258})
